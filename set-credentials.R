1a94007aa9259faf5ba1f59a60f871ff3412b6fd
guadag12 <guadagonz12@gmail.com>
2024-08-21 15:53:48 -0300

    Initial commit

---
+++ set-credentials.R
@@ -0,0 +1,9 @@
+
+# Set credentials ---------------------------------------------------------
+
+library(usethis)
+use_git_config(user.name="guadag12", user.email="guadagonz12@gmail.com")
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+
